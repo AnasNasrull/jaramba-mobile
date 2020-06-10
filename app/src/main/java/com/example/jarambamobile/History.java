fc08a026c197e2826fe8128b80985ec2712debff
M Anas Nasrulloh <AnasNasrull@users.noreply.github.com>
2020-06-10 16:15:56 +0700

    update rate star

--- app/src/main/java/com/example/jarambamobile/History.java
+++ app/src/main/java/com/example/jarambamobile/History.java
@@ -2,23 +2,51 @@ package com.example.jarambamobile;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class History extends AppCompatActivity {
     private ImageView info;
     private ImageView rate;
+    private TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        info = findViewById(R.id.iconinfo);
+        info = findViewById(R.id.iconinfo1);
         rate = findViewById(R.id.rate);
+        status = findViewById(R.id.status_list);
+
+        status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu dropDownMenu = new PopupMenu(getApplicationContext(), status);
+                dropDownMenu.getMenuInflater().inflate(R.menu.list_status, dropDownMenu.getMenu());
+                dropDownMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        //Toast.makeText(getApplicationContext(), "You have clicked " + menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                });
+                dropDownMenu.show();
+            }
+        });
 
         info.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,14 +54,6 @@ public class History extends AppCompatActivity {
                 final Dialog dialog1 = new Dialog(History.this);
 
                 dialog1.setContentView(R.layout.history_detail);
-
-                ImageView back = dialog1.findViewById(R.id.icon_back1);
-                back.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog1.dismiss();
-                    }
-                });
 
                 dialog1.show();
             }
@@ -46,10 +66,24 @@ public class History extends AppCompatActivity {
 
                 dialog.setContentView(R.layout.history_rating);
 
-                ImageView back = dialog.findViewById(R.id.icon_back2);
-                back.setOnClickListener(new View.OnClickListener() {
+                Button Submit = dialog.findViewById(R.id.submit_rate);
+                final RatingBar Rating = dialog.findViewById(R.id.ratingBar);
+                final EditText Komentar = dialog.findViewById(R.id.comment_rate);
+                final TextView Harga = dialog.findViewById(R.id.harga_rate);
+                final TextView Pembayaran = dialog.findViewById(R.id.crbyr_rate);
+
+                Submit.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        String rating = "Rating is : " + Rating.getRating() +"\nKomentar : " + Komentar.getText();
+
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference myRef = database.getReference("history_user_app");
+
+                        myRef.setValue(new history_rating_data(Rating.getRating(), Komentar.getText().toString(), Harga.getText().toString(), Pembayaran.getText().toString()));
+
+                        Toast.makeText(History.this, rating, Toast.LENGTH_LONG).show();
+
                         dialog.dismiss();
                     }
                 });
@@ -57,6 +91,8 @@ public class History extends AppCompatActivity {
                 dialog.show();
             }
         });
+
+
 
     }
 }
