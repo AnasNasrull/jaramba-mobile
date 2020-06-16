c73eaadf88ef165783e4cb5545bafdd945c0a2cc
bagasbest <bagasbest80@gmail.com>
2020-06-16 11:14:34 +0700

    update fonts to NuritoSans, regular and semiBold, and Create UI for Damri Start Trip Page

---
+++ app/src/main/java/com/example/jarambamobile/DamriStartTrip.java
@@ -0,0 +1,27 @@
+package com.example.jarambamobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class DamriStartTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_damri_start_trip);
+    }
+
+    public void letsGo(View view) {
+
+    }
+
+    public void addMetodePembayaran(View view) {
+
+    }
+
+    public void addJumlahPenumpang(View view) {
+
+    }
+}
